3166c64d958d3570dd1bb7192108694fd3a0a503
VCarames <109115310+VCarames@users.noreply.github.com>
2024-05-31 16:55:38 -0700

    Create BookmarkMovieList.jsx

---
+++ src/components/lists/BookmarkMovieList.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import mediaData from "/public/data/media.json";
+import { Link } from "react-router-dom";
+
+function BookmarkMovieList() {
+  const bookmarkedMovies = mediaData.filter(
+    (item) => item.isBookmarked && item.category === "Movie"
+  );
+
+  return (
+    <div className="bookmark__category">
+      <h2 className="bookmark__heading">Bookmarked Movies</h2>
+      <ul className="thumbnail__list" role="list">
+        {bookmarkedMovies.map((movie) => (
+          <li className="thumbnail" key={movie.title} role="listitem">
+            <picture className="thumbnail__picture">
+              <source
+                media="(min-width: 1000px)"
+                srcSet={movie.thumbnail.regular.large}
+              />
+              <source
+                media="(min-width: 760px)"
+                srcSet={movie.thumbnail.regular.medium}
+              />
+              <img
+                src={movie.thumbnail.regular.small}
+                alt={`${movie.title} thumbnail`}
+              />
+            </picture>
+            <div className="thumbnail__content">
+              <button className="thumbnail__bookmark">
+                {movie.isBookmarked ? (
+                  <img
+                    src="/src/assets/icons/icon-bookmark-full.svg"
+                    alt="Bookmarked"
+                  />
+                ) : (
+                  <img
+                    src="/src/assets/icons/icon-bookmark-empty.svg"
+                    alt="Not Bookmarked"
+                  />
+                )}
+              </button>
+              <div className="thumbnail__category--info">
+                <p className="thumbnail-year">{movie.year}</p>
+                <div className="thumbnail__container--category">
+                  <img src={movie.icon} alt="" />
+                  <p className="thumbnail__category">{movie.category}</p>
+                </div>
+                <p className="thumbnail__rating">{movie.rating}</p>
+              </div>
+              <h3 className="thumbnail__heading">
+                <Link
+                  className="thumbnail__link"
+                  to={movie.category === "Movie" ? "/movies" : "/tv-series"}
+                >
+                  {movie.title}
+                </Link>
+              </h3>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default BookmarkMovieList;
