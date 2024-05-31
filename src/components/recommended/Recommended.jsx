3e9d018f4b7d93d5e4b31a3f6d6993b3b77605b7
VCarames <109115310+VCarames@users.noreply.github.com>
2024-05-31 16:01:19 -0700

    Create Recommended.jsx

---
+++ src/components/recommended/Recommended.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import RecommendedList from "./RecommendedList";
+
+function Recommended() {
+  return (
+    <section className="recommended">
+      <h2 className="recommended__heading">Recommended for you</h2>
+      <RecommendedList />
+    </section>
+  );
+}
+
+export default Recommended;
