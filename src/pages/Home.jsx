39d4167e801f51ebbb4850e77b3dbe2be780fbb6
VCarames <109115310+VCarames@users.noreply.github.com>
2024-05-31 15:59:36 -0700

    Trending Component Added to "Home" Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../components/header/Header";
 import SearchBar from "../components/searchbar/Searchbar";
+import Trending from "../components/trending/Trending";
 
 function Home() {
   return (
@@ -10,6 +11,9 @@ function Home() {
         labelText="Search for movies or TV series"
         placeholderText="Search for movies or TV series"
       />
+      <main>
+        <Trending />
+      </main>
     </div>
   );
 }
