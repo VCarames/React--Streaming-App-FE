520896c4f0b9b5ad5d73b59ddc95e8553e95ffea
VCarames <109115310+VCarames@users.noreply.github.com>
2024-05-31 22:10:58 -0700

    Search Results Component Added to "Search" Page

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,24 +1,29 @@
 import React from "react";
+import { useSearch } from "/src/components/searchbar/SearchContext.jsx";
 import Header from "../components/header/Header";
 import SearchBar from "/src/components/searchbar/Searchbar.jsx";
+import SearchResults from "/src/components/searchbar/SearchResults.jsx";
 
-function Search() {
+const Search = () => {
+  const { searchResults, searchQuery } = useSearch();
+
   return (
-    <div>
-      <div className="layout">
-        <Header />
-        <SearchBar
-          labelText="Search for movies or TV series"
-          placeholderText="Search for movies or TV series"
-        />
-        <main>
-          <section className="search">
-            <h2 className="search__heading"></h2>
-          </section>
-        </main>
-      </div>
+    <div className="layout">
+      <Header />
+      <SearchBar
+        labelText="Search for movies or TV series"
+        placeholderText="Search for movies or TV series"
+      />
+      <main>
+        <section className="search">
+          <h2 className="search__heading">
+            Found {searchResults.length} results for {searchQuery}
+          </h2>
+          <SearchResults results={searchResults} />
+        </section>
+      </main>
     </div>
   );
-}
+};
 
 export default Search;
