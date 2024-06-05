3d5e5c3d26ecd913dada994b1b50143d07c2d3cb
VCarames <109115310+VCarames@users.noreply.github.com>
2024-06-04 22:10:28 -0700

    Update TVSeries.jsx

--- src/pages/TVSeries.jsx
+++ src/pages/TVSeries.jsx
@@ -6,7 +6,6 @@ import TVSeriesList from "../components/lists/TVSeriesList.jsx";
 function TVSeries() {
   return (
     <div className="layout">
-      <h1 className="tvseries__heading">TV Series Page</h1>
       <Header />
       <SearchBar
         labelText="Search for TV series"
@@ -14,6 +13,7 @@ function TVSeries() {
       />
       <main>
         <section className="tvseries">
+          <h1 className="tvseries__heading">TV Series</h1>
           <TVSeriesList />
         </section>
       </main>
